82326462d5495df339b7cc8afc7c41947abd2aad
qyy <rizqyalbani@gmail.com>
2020-02-25 23:23:33 +0800

    pembuatan input data, penambahan view template

--- app/controllers/Admin.php
+++ app/controllers/Admin.php
@@ -2,6 +2,7 @@
     class Admin extends mainController{
         public function index(){
             $this->view("templates/header");
+            $this->view("admin/index");
             $this->view("templates/footer");
         }
     }
---
+++ app/view/admin/index.php
@@ -0,0 +1,33 @@
+<h1>Hello This is admin</h1>
+<!-- Nomor surat masuk -->
+<form action="">
+    <div>
+        <label for="nmr_srt_msk">Nomor surat masuk</label>
+        <input id="nmr_srt_msk" name="nmr_srt_msk" type="text">
+    </div>
+    <!-- Tanggal surat masuk -->
+    <div>
+        <label for="tgl_srt_msk">Tanggal surat masuk</label>
+        <input id="tgl_srt_msk" name="tgl_srt_msk" type="text">
+    </div>
+    <!-- Alamat Pengirim -->
+    <div>
+        <label for="alamat_srt_msk">Alamat Pengirim</label>
+        <input id="alamat_srt_msk" name="alamat_srt_msk" type="text">
+    </div>
+    <!-- perihal -->
+    <div>
+        <label for="perihal_srt_msk">Perihal Surat Masuk</label>
+        <input id="perihal_srt_msk" name="perihal_srt_msk" type="text">
+    </div>
+    <!-- lampiran dokumen -->
+    <div>
+        <label for="lampiran_srt_msk">Lampiran Surat Masuk</label>
+        <input id="lampiran_srt_msk" name="lampiran_srt_msk" type="text">
+    </div>
+    <!-- disposisi surat msk -->
+    <div>
+        <label for="disposisi_srt_msk">Disposisi Surat Masuk</label>
+        <input id="disposisi_srt_msk" name="disposisi_srt_msk" type="text">
+    </div>
+</form>
